b28f02379a4e8842422e5906b6f884d6f969322a
Carman-Tim <timothy.carman@heig-vd.ch>
2022-12-08 17:38:34 +0100

    Better

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <memory>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -61,20 +62,75 @@ public:
     int execute(vector<int> v)
     {
         cout << "Subb" << endl;
-        return std::accumulate(v.begin(), v.end(), 0);
+        return std::accumulate(v.begin(), v.end(), 0, minus<int>());
     }
 };
+
+class ConcreteMultiply : public Strategy
+{
+public:
+    int execute(vector<int> v)
+    {
+        cout << "Multiply" << endl;
+        return std::accumulate(v.begin(), v.end(), 1, multiplies<int>());
+    }
+};
+
+class ConcreteMax : public Strategy
+{
+public:
+    int execute(vector<int> v)
+    {
+        cout << "Max" << endl;
+        return *std::max_element(v.begin(), v.end());
+    }
+};
+
+class ConcreteMin : public Strategy
+{
+public:
+    int execute(vector<int> v)
+    {
+        cout << "Min" << endl;
+        return *std::min_element(v.begin(), v.end());
+    }
+};
+
 
 int main()
 {
     Context context;
 
     context.add_strategy("add", new ConcreteAdd);
-    context.add_strategy("multiply", new ConcreteSub);
+    context.add_strategy("sub", new ConcreteSub);
+    context.add_strategy("multiply", new ConcreteMultiply);
+    context.add_strategy("max", new ConcreteMax);
+    context.add_strategy("min", new ConcreteMin);
+
 
     vector<int> v = {1, 2, 3, 4, 5, 6, 7};
 
     if (context.set_strategy("add"))
+        std::cerr << "Error: no valid strategy";
+
+    cout << context.execute(v) << endl;
+
+    if (context.set_strategy("sub"))
+        std::cerr << "Error: no valid strategy";
+
+    cout << context.execute(v) << endl;
+
+    if (context.set_strategy("multiply"))
+        std::cerr << "Error: no valid strategy";
+
+    cout << context.execute(v) << endl;
+
+    if (context.set_strategy("max"))
+        std::cerr << "Error: no valid strategy";
+
+    cout << context.execute(v) << endl;
+
+    if (context.set_strategy("min"))
         std::cerr << "Error: no valid strategy";
 
     cout << context.execute(v) << endl;
